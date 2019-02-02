c93b96a888934431a4ff8d1e66f884dd7fbc9d39
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-02 15:05:51 +0200

    Add files via upload

---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/03. OldestFamilyMember/Family.cs
@@ -0,0 +1,26 @@
+﻿namespace DefiningClasses
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Family
+    {
+        private List<Person> people;
+
+        public List<Person> People
+        {
+            get { return this.people; }
+            set { this.people = value; }
+        } 
+
+        public void AddMember(Person member)
+        {
+            this.people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.people.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+    }
+}
