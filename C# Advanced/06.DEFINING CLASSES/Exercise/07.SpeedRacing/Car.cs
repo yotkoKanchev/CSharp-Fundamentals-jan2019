85cd50100748dc22891026dc3b5dd5b91b9383b4
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-02 15:00:22 +0200

    Add files via upload

---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/07.SpeedRacing/Car.cs
@@ -0,0 +1,25 @@
+﻿namespace _07.SpeedRacing
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumption { get; set; }
+        public double TraveledDistance { get; set; }
+
+        public void CheckDistance( double distance)
+        {
+            double amountOfFuelNeeded = this.FuelConsumption * distance;
+
+            if (this.FuelAmount >= amountOfFuelNeeded)
+            {
+                this.FuelAmount -= amountOfFuelNeeded;
+                this.TraveledDistance += distance;
+            }
+            else
+            {
+                System.Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
