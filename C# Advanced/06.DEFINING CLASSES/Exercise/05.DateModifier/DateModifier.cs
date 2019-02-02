0c739a4bbbf3bcbc5b69f99b65adb8e5362c4f29
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-02 15:03:29 +0200

    Add files via upload

---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.DateModifier
+{
+    public class DateModifier
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public DateModifier(DateTime start, DateTime end)
+        {
+            this.StartDate = start;
+            this.EndDate = end;
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        public double GetDaysDifference()
+        {
+            return Math.Abs((EndDate - startDate).TotalDays);
+        }
+    }
+}
