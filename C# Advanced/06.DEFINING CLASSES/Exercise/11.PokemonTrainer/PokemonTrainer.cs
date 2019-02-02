3360fd876ffd87b7dad17f8ac3a5ea9c01255c45
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-02 20:53:45 +0200

    Add files via upload

---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/11.PokemonTrainer/PokemonTrainer.cs
@@ -0,0 +1,100 @@
+﻿namespace _11.PokemonTrainer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PokemonTrainer
+    {
+        static void Main(string[] args)
+        {
+            List<Pokemon> pokemons = new List<Pokemon>();
+            List<Trainer> trainers = new List<Trainer>();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine?.ToLower() != "tournament")
+            {
+                string[] tokens = inputLine.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string trainerName = tokens[0];
+                string pokemonName = tokens[1];
+                string pokemonElement = tokens[2];
+                int pokemonHealth = int.Parse(tokens[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                bool trainerExist = false;
+
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Name == trainerName)
+                    {
+                        trainerExist = true;
+                        trainers.Find(x => x.Name == trainerName).PokemonsList.Add(pokemon);
+                        break;
+                    }
+                }
+
+                if (trainerExist == false)
+                {
+                    List<Pokemon> currentPokList = new List<Pokemon>();
+                    currentPokList.Add(pokemon);
+                    Trainer trainer = new Trainer(trainerName, 0, currentPokList);
+                    trainers.Add(trainer);
+                }
+                inputLine = Console.ReadLine();
+            }
+
+            string askedElement = Console.ReadLine();
+
+            while (askedElement?.ToLower() != "end")
+            {
+                trainers = CheckElementExist(askedElement, trainers);
+
+                askedElement = Console.ReadLine();
+            }
+
+            foreach (var trainer in trainers.OrderByDescending(x=>x.NumberOfBadges))
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.PokemonsList.Count()}");
+            }
+        }
+
+        public static List<Trainer> CheckElementExist(string element, List<Trainer> trainers)
+        {
+            foreach (var currentTrainer in trainers)
+            {
+                bool exist = false;
+
+                foreach (var pokemon in currentTrainer.PokemonsList)
+                {
+                    if (pokemon.Element == element)
+                    {
+                        exist = true;
+                        currentTrainer.NumberOfBadges += 1;
+                        break;
+                    }
+                }
+                List<Pokemon> pokemonsToRemove = new List<Pokemon>();
+
+                if (exist == false)
+                {
+                    foreach (var pokemon in currentTrainer.PokemonsList)
+                    {
+                        pokemon.Health -= 10;
+                        if (pokemon.Health <= 0)
+                        {
+                            pokemonsToRemove.Add(pokemon);
+                        }
+                    }
+                }
+
+                foreach (var pokemon in pokemonsToRemove)
+                {
+                    currentTrainer.PokemonsList.Remove(pokemon);
+                }
+            }
+            return trainers;
+        }
+    }
+}
---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/11.PokemonTrainer/Trainer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace _11.PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name, int badgesCount, List<Pokemon> pokemons)
+        {
+            this.Name = name;
+            this.NumberOfBadges = badgesCount;
+            this.PokemonsList = pokemons;
+        }
+
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public List<Pokemon> PokemonsList { get; set; } = new List<Pokemon>();       
+    }      
+}
