b20636b5abfc577debe3cbd2aa35bc7ec7fbeb9d
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-03 01:35:43 +0200

    Add files via upload

---
+++ C# Advanced/06.DEFINING CLASSES/Lab/3. Car Constructors/Car.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+        public Car(string make, string model, int year)
+            :this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return this.fuelQuantity; }
+            set { this.fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return this.fuelConsumption; }
+            set { this.fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            var consumedFuel = distance * this.fuelConsumption;
+            if (this.fuelQuantity - consumedFuel >= 0)
+            {
+                this.fuelQuantity -= consumedFuel;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.make}");
+            sb.AppendLine($"Model: {this.model}");
+            sb.AppendLine($"Year: {this.year}");
+            sb.Append($"Fuel: {this.fuelQuantity:f2}L");
+            return sb.ToString();
+        }
+    }
+}
