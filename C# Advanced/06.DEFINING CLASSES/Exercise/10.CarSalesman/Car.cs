6b95a3862ce43139c49ece82f4ffa11eb3171a1f
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-02 17:09:20 +0200

    Add files via upload

---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/10.CarSalesman/Car.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace _10.CarSalesman
+{
+    public class Car
+    {
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public string Weight { get; set; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"{this.Model}:");
+            stringBuilder.AppendLine(Engine.ToString());
+            stringBuilder.AppendLine($"  Weight: {this.Weight}");
+            stringBuilder.Append($"  Color: {this.Color}");
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
