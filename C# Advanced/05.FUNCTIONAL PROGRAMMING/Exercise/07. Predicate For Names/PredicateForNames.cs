ef361c2ced287e0d91723485fec30740e709b5dc
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-05 11:02:07 +0200

    Add files via upload

---
+++ C# Advanced/05.FUNCTIONAL PROGRAMMING/Exercise/07. Predicate For Names/PredicateForNames.cs
@@ -0,0 +1,23 @@
+﻿namespace _07._Predicate_For_Names
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class PredicateForNames
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string[], string[]> filteredNames = peopleNames => peopleNames.Where(x => x.Length <= nameLength).ToArray();
+            Action<string> print = name => Console.WriteLine(name);
+
+            foreach (var name in filteredNames(names))
+            {
+                print(name);    
+            }
+        }
+    }
+}
