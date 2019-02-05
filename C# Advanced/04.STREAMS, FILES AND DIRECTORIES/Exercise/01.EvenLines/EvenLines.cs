c96793805d5d82847516e3c514281a24e59c501d
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-05 10:49:33 +0200

    Add files via upload

---
+++ C# Advanced/04.STREAMS, FILES AND DIRECTORIES/Exercise/01.EvenLines/EvenLines.cs
@@ -0,0 +1,45 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    class evenLines
+    {
+        static void Main(string[] args)
+        {
+            char[] symbolsToReplace = new char[] {'-',',','.','!','?'};
+            int counter = 0;
+
+            using (var reader = new StreamReader(@"..\..\..\text.txt"))
+            {       
+                using (var writer = new StreamWriter(@"..\..\..\output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    
+                    while (line != null)
+                    {
+                        if (counter % 2 == 0)
+                        {
+                            for (int i = 0; i < line.Length; i++)
+                            {
+                                if (symbolsToReplace.Contains(line[i]))
+                                {
+                                    line = line.Replace(line[i], '@');
+                                }
+                            }
+                            
+                            string[] lineAsArray = line.Split();
+                            Array.Reverse(lineAsArray);
+                            line = string.Join(" ", lineAsArray);
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
+
