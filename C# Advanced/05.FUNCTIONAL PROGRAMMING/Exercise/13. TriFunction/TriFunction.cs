8f644cbcd8a5f82eb9412bed96e6e60d1d770274
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-05 11:10:11 +0200

    Add files via upload

---
+++ C# Advanced/05.FUNCTIONAL PROGRAMMING/Exercise/13. TriFunction/TriFunction.cs
@@ -0,0 +1,22 @@
+﻿namespace _13._TriFunction
+{
+    using System;
+    using System.Linq;
+
+    class TriFunction
+    {
+        static void Main(string[] args)
+        {
+            int sumToCompare = int.Parse(Console.ReadLine());
+
+             string name = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.ToCharArray()
+                .Select(y=>(int)y)
+                .Sum() >= sumToCompare)
+                .FirstOrDefault();
+
+            Console.WriteLine(name);
+        }
+    }
+}
