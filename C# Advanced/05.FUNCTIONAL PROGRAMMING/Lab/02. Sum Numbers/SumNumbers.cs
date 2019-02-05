bad87ca452def753e46ec6656c95cc131dcc3b8f
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-05 10:55:50 +0200

    Add files via upload

---
+++ C# Advanced/05.FUNCTIONAL PROGRAMMING/Lab/02. Sum Numbers/SumNumbers.cs
@@ -0,0 +1,20 @@
+﻿namespace _02._Sum_Numbers
+{
+    using System;
+    using System.Linq;
+
+    class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = n => int.Parse(n);
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
