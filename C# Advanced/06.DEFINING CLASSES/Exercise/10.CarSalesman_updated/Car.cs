028a9a17b34c8c7fe834f33cf3567c9c40e5ad2b
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-11 00:15:28 +0200

    Add files via upload

---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/10.CarSalesman_updated/Car.cs
@@ -0,0 +1,54 @@
+﻿namespace _10.CarSalesman
+{
+    using System.Text;
+    using System;
+
+    public class Car
+    {
+        private string model;
+        private Engine engine;
+        private int weight;
+        private string color;
+
+        public Car(string model, Engine engine, int weight, string color)
+        {
+            this.model = model;
+            this.engine = engine;
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public Car(string model, Engine engine, int weight)
+            : this(model, engine, weight, "n/a")
+        {
+        }
+
+        public Car(string model, Engine engine, string color)
+            : this(model, engine, -1, color)
+        {
+        }
+
+        public Car(string model, Engine engine)
+            : this(model, engine, -1, "n/a")
+        {
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.model}:");
+            sb.AppendLine(engine.ToString());
+            if (weight == -1)
+            {
+                sb.AppendLine($"  Weight: n/a");
+            }
+            else
+            {
+                sb.AppendLine($"  Weight: {this.weight}");
+            }
+            sb.Append($"  Color: {this.color}");
+
+            return sb.ToString();
+        }
+    }
+}
