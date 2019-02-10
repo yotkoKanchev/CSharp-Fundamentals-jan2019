bf181893c8614fe8708091bccab0be33f3593815
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-11 00:13:36 +0200

    Add files via upload

---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/12.Google_updated/Google.cs
@@ -0,0 +1,71 @@
+﻿namespace _12.Google
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Google
+    {
+        public static void Main()
+        {
+            List<Person> people = new List<Person>();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine?.ToLower() != "end")
+            {
+                string[] tokens = inputLine.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string personName = tokens[0];
+
+                if (people.Any(x=>x.Name == personName))
+                {
+                    Person currentPerson = people.Find(x => x.Name == personName);
+                    AddPersonInformation(currentPerson, tokens);                    
+                }
+                else
+                {
+                    Person person = new Person(tokens[0]);
+                    AddPersonInformation(person, tokens);
+                    people.Add(person);
+                }               
+
+                inputLine = Console.ReadLine();
+            }
+
+            string askedPerson = Console.ReadLine();
+
+            Console.WriteLine(people.FirstOrDefault(x=>x.Name == askedPerson));
+        }
+
+        public static void AddPersonInformation(Person currentPerson, string[] tokens)
+        {
+            if (tokens[1] == "company")
+            {
+                Company company = new Company(tokens[2], tokens[3], decimal.Parse(tokens[4]));
+                currentPerson.Company = company;
+            }
+            else if (tokens[1] == "car")
+            {
+                Car car = new Car(tokens[2], int.Parse(tokens[3]));
+                currentPerson.Car = car;
+            }
+            else if (tokens[1] == "pokemon")
+            {
+                Pokemon pokemon = new Pokemon(tokens[2], tokens[3]);
+                currentPerson.AddPokemon(pokemon);
+            }
+            else if (tokens[1] == "parents")
+            {
+                Person parent = new Person(tokens[2]);
+                parent.DateOfBirth = tokens[3];
+                currentPerson.AddParent(parent);
+            }
+            else if (tokens[1] == "children")
+            {
+                Person child = new Person(tokens[2]);
+                child.DateOfBirth = tokens[3];
+                currentPerson.AddChild(child);
+            }
+        }
+    }
+}
---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/12.Google_updated/Person.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _12.Google
+{
+    public class Person
+    {
+        private string name;
+        private string birthDay;
+        private Car car;
+        private Company company;
+        private List<Pokemon> pokemons = new List<Pokemon>();
+        private List<Person> parents = new List<Person>();
+        private List<Person> children = new List<Person>();
+                
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public Car Car { get { return this.car; } set { this.car = value; } }
+        public Company Company { get { return this.company; } set { this.company = value; } }
+        public string Name { get { return this.name; } }
+        public string DateOfBirth { get { return this.birthDay; } set { this.birthDay = value; } }
+
+        public void AddPokemon(Pokemon pokemon)
+        {
+            this.pokemons.Add(pokemon);
+        }
+
+        public void AddChild(Person child)
+        {
+            this.children.Add(child);
+        }
+
+        public void AddParent(Person parent)
+        {
+            this.parents.Add(parent);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Name}");
+            sb.AppendLine($"Company:");
+            if (this.Company != null)
+            {
+                sb.AppendLine(this.Company.ToString());
+            }
+            sb.AppendLine($"Car:");
+            if (this.Car != null)
+            {
+                sb.AppendLine(this.Car.ToString());
+            }
+            sb.AppendLine($"Pokemon:");            
+            if (this.pokemons.Any())
+            {
+                foreach (var pokemon in this.pokemons)
+                {
+                    sb.AppendLine(pokemon.ToString());
+                }
+            }
+            sb.AppendLine($"Parents:");
+            if (this.parents.Any())
+            {
+                foreach (var parent in this.parents)
+                {
+                    sb.AppendLine($"{parent.Name} {parent.birthDay}");
+                }
+            }
+            sb.AppendLine($"Children:");
+            if (this.children.Any())
+            {
+                foreach (var child in this.children)
+                {
+                    sb.AppendLine($"{child.Name} {child.birthDay}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
