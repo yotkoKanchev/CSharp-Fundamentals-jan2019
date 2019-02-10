84963fdfb825e3ac4a4dc987aebfbe115b71e208
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-11 00:14:29 +0200

    Add files via upload

---
+++ C# Advanced/06.DEFINING CLASSES/Exercise/11.PokemonTrainer_updated/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿namespace _11.PokemonTrainer
+{
+    public class Pokemon
+    {
+        private string name;
+        private string element;
+        private int health;
+
+        public Pokemon(string name, string element, int health)
+        {
+            this.name = name;
+            this.element = element;
+            this.health = health;
+        }
+
+        public string Element { get { return this.element; } }
+        public int Health { get { return this.health; } set { this.health = value; } }
+    }
+}
