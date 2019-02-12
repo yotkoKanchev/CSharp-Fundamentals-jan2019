cf97a62f9c0af097f2d2d26ded4b4a91a437b0dc
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-12 10:52:37 +0200

    Add files via upload

---
+++ C# Advanced/07.WORKSHOP/07. Workshop Lab/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CreateLinkedList
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+                        
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddFirst(3);
+            list.AddFirst(5);
+            list.AddFirst(6);
+            list.AddFirst(7);
+            list.AddFirst(8);
+            list.RemoveLast();
+            list.RemoveFirst();
+            list.ForEach(x => Console.WriteLine(2));
+
+            var array = list.ToArray();
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(list.Count);
+        }
+    }
+}
