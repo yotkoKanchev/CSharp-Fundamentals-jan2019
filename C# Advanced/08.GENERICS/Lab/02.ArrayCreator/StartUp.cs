5f4b780e76107710bc4829249195154de7167298
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-18 10:15:27 +0200

    Add files via upload

---
+++ C# Advanced/08.GENERICS/Lab/02.ArrayCreator/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericArrayCreator
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+
+            string[] input = Console.ReadLine().Split();
+        }
+    }
+}
