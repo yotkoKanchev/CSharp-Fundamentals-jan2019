d5001e72e42aad162ace4f9ad5b28e7f68d015fc
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-18 10:14:47 +0200

    Add files via upload

---
+++ C# Advanced/08.GENERICS/Lab/01.Box/Box.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public List<T> DataList { get; set; } = new List<T>();
+
+        public int Count => this.DataList.Count;
+
+        public void Add(T element)
+        {
+            this.DataList.Add(element);
+        }
+
+        public T Remove()
+        {
+            var element = DataList[this.Count - 1];
+            this.DataList.RemoveAt(this.Count - 1);
+            return element;
+        }
+    }
+}
