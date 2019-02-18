2445cb56131f8172a5bf9f4a5318812bca25a1f2
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-18 10:17:13 +0200

    Add files via upload

---
+++ C# Advanced/08.GENERICS/Exercises/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $" {this.item.GetType()}: {this.item}";
+        }
+    }
+}
---
+++ C# Advanced/08.GENERICS/Exercises/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.GenericBoxOfString
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int linesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                string data = Console.ReadLine();
+                Box<string> box = new Box<string>(data);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
