0e2d8f7183baa2da472608ca284d951f0d245101
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-18 10:18:04 +0200

    Add files via upload

---
+++ C# Advanced/08.GENERICS/Exercises/02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace _02.GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private T number;
+
+        public Box(T number)
+        {
+            this.number = number;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.number.GetType()}: {this.number}";
+        }
+    }
+}
---
+++ C# Advanced/08.GENERICS/Exercises/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.GenericBoxOfInteger
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int inputLinesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputLinesCount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(number);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
