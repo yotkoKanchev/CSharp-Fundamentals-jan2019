da21d9fa6d1bbd6fc2ae931321d6ec8d3412be7d
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-18 10:20:34 +0200

    Add files via upload

---
+++ C# Advanced/08.GENERICS/Exercises/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        public Box(List<T> elements)
+        {
+            this.Elements = elements;
+        }
+
+        public List<T> Elements { get; set; } = new List<T>();
+
+        public int GetCount(T elementToCompareWith)
+        {
+            int counter = 0;
+
+            foreach (var element in this.Elements)
+            {
+                if (element.CompareTo(elementToCompareWith) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ C# Advanced/08.GENERICS/Exercises/06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _06.GenericCountMethodDouble
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int elementsCount = int.Parse(Console.ReadLine());
+
+            List<double> elements = new List<double>();
+
+            for (int i = 0; i < elementsCount; i++)
+            {
+                elements.Add(double.Parse(Console.ReadLine()));
+            }
+
+            double comperisonElement = double.Parse(Console.ReadLine());
+
+            var box = new Box<double>(elements);
+
+            Console.WriteLine(box.GetCount(comperisonElement));
+        }
+    }
+}
