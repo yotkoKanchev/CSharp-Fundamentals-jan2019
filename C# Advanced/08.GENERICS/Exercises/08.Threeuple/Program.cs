0a378422012213bf944ad59a26515afea5524f64
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-18 10:21:41 +0200

    Add files via upload

---
+++ C# Advanced/08.GENERICS/Exercises/08.Threeuple/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _08.Threeuple
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] personInput = Console.ReadLine().Split(" ");
+
+            string name = personInput[0] + " " + personInput[1];
+            string address = personInput[2];
+            string town = personInput[3];
+
+            var personThreeuple = new Threeuple<string,string,string>(name, address, town);
+
+            string[] beerInput = Console.ReadLine().Split(" ");
+
+            string personName = beerInput[0];
+            int litters = int.Parse(beerInput[1]);
+            bool isDrunk = false;
+
+            if (beerInput[2] == "drunk")
+            {
+                isDrunk = true;
+            }
+
+            var beerThreeuple = new Threeuple<string, int, bool>(personName, litters, isDrunk);
+
+            string[] accountInput = Console.ReadLine().Split(" ");
+
+            string clientName = accountInput[0];
+            double balance = double.Parse(accountInput[1]);
+            string bank = accountInput[2];
+
+            var accountThreeuple = new Threeuple<string, double, string>(clientName, balance, bank);
+
+            Console.WriteLine(personThreeuple);
+            Console.WriteLine(beerThreeuple);
+            Console.WriteLine(accountThreeuple);
+        }
+    }
+}
---
+++ C# Advanced/08.GENERICS/Exercises/08.Threeuple/Threeuple.cs
@@ -0,0 +1,22 @@
+﻿namespace _08.Threeuple
+{
+    public class Threeuple<I1, I2, I3>
+    {
+        public Threeuple(I1 item1, I2 item2, I3 item3)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+        }
+
+        public I1 Item1 { get; set; }
+        public I2 Item2 { get; set; }
+        public I3 Item3 { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2} -> {this.Item3}";
+        }
+    }
+}
