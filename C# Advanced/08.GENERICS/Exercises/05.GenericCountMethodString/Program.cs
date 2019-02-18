73828fea9957fe06dbe4bb69275699f3aa56fc71
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-18 10:20:00 +0200

    Add files via upload

---
+++ C# Advanced/08.GENERICS/Exercises/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.GenericCountMethodString
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int elementsCount = int.Parse(Console.ReadLine());
+
+            List<string> elements = new List<string>();
+
+            for (int i = 0; i < elementsCount; i++)
+            {
+                elements.Add(Console.ReadLine());
+            }
+
+            string comperisonElement = Console.ReadLine();
+
+            var box = new Box<string>(elements);
+
+            Console.WriteLine(box.GetCount(comperisonElement));
+        }
+    }
+}
