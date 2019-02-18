a507b5fbe580c7102dafc61f65c678d233e3693d
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-18 10:19:17 +0200

    Add files via upload

---
+++ C# Advanced/08.GENERICS/Exercises/04.GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Box <T>
+    {
+        public T FirstElement { get; set; }
+        public T SecondElement { get; set; }
+        public List<T> Elements { get; set; } = new List<T>();
+
+        public void SwapElements(int firstIndex, int secondIndex)
+        {
+            T temp = this.Elements[firstIndex];
+            this.Elements[firstIndex] = this.Elements[secondIndex];
+            this.Elements[secondIndex] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var element in this.Elements)
+            {
+               sb.AppendLine($"{element.GetType()}: {element}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/08.GENERICS/Exercises/04.GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.GenericSwapMethodInteger
+{
+    using _03.GenericSwapMethodString;
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int boxesCount = int.Parse(Console.ReadLine());
+
+            Box<int> boxes = new Box<int>();
+
+            for (int i = 0; i < boxesCount; i++)
+            {
+                boxes.Elements.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            boxes.SwapElements(indexes[0], indexes[1]);
+            Console.WriteLine(boxes);
+        }
+    }
+}
