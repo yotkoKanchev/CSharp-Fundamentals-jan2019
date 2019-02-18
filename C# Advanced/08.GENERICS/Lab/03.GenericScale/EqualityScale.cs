2054ed76b4d01e68d04ec80133ad4940b1909277
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2019-02-18 10:16:14 +0200

    Add files via upload

---
+++ C# Advanced/08.GENERICS/Lab/03.GenericScale/EqualityScale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T : IComparable<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public bool AreEqual()
+        {
+            if (this.left.CompareTo(this.right) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
