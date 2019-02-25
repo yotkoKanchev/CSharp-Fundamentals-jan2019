9aa7879d002fdd4312c68ae1db5a29e8e6d053d3
yotkoKanchev <yotko.kanchev@gmail.com>
2019-02-25 09:00:30 +0200

    renamed some folder and deleted some files
    s

---
+++ C# Advanced/00.C# Advanced Exam - 24feb2019/03.Heroes/HeroRepository.cs
@@ -0,0 +1,66 @@
+﻿namespace Heroes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class HeroRepository
+    {
+        private List<Hero> data;
+
+        public HeroRepository()
+        {
+            this.data = new List<Hero>();
+        }
+
+        public int Count { get { return this.data.Count; } set { } }
+
+        public void Add(Hero hero)
+        {
+            this.data.Add(hero);
+        }
+
+        public void Remove(string name)
+        {
+            Hero currentHero = data.FirstOrDefault(x => x.Name == name);
+            this.data.Remove(currentHero);
+        }
+
+        public Hero GetHeroWithHighestStrength()
+        {
+            return this.data.OrderByDescending(x => x.Item.Strength).FirstOrDefault();
+        }
+
+        public Hero GetHeroWithHighestAbility()
+        {
+            return this.data.OrderByDescending(x => x.Item.Ability).FirstOrDefault();
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            return this.data.OrderByDescending(x => x.Item.Intelligence).FirstOrDefault();
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (i != data.Count - 1)
+                {
+                    stringBuilder.AppendLine(data[i].ToString());
+                }
+                else
+                {
+                    stringBuilder.Append(data[i].ToString());
+                }
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/00.C# Advanced Exam - 24feb2019/03.Heroes/Item.cs
@@ -0,0 +1,36 @@
+﻿namespace Heroes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Item
+    {
+        private int strength;
+        private int ability;
+        private int intelligence;
+
+        public Item(int strength, int ability, int intelligence)
+        {
+            this.Strength = strength;
+            this.Ability = ability;
+            this.Intelligence = intelligence;
+        }
+
+        public int Strength { get => strength; set => strength = value; }
+        public int Ability { get => ability; set => ability = value; }
+        public int Intelligence { get => intelligence; set => intelligence = value; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("Item:");
+            stringBuilder.AppendLine($"  * Strength: {this.Strength}");
+            stringBuilder.AppendLine($"  * Ability: {this.Ability}");
+            stringBuilder.Append($"  * Intelligence: {this.Intelligence}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
