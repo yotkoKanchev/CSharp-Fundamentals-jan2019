1deb8cd205a4db55ab891a410d3f616e01879f49
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-10-27 19:14:08 +0200

    Add files via upload

---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/06.EqualityLogic/EqualityLogic.cs
@@ -0,0 +1,30 @@
+﻿namespace P06EqualityLogic
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EqualityLogic
+    {
+        public static void Main()
+        {
+            var sortedSet = new SortedSet<Person>();
+            var set = new HashSet<Person>();
+
+            var count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var args = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var name = args[0];
+                var age = int.Parse(args[1]);
+
+                var person = new Person(name, age);
+                sortedSet.Add(person);
+                set.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(set.Count);
+        }
+    }
+}
---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/06.EqualityLogic/Person.cs
@@ -0,0 +1,45 @@
+﻿namespace P06EqualityLogic
+{
+    using System;
+
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Person;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Name.Equals(other.Name) && this.Age.Equals(other.Age);
+        }
+
+        public override int GetHashCode()
+        {
+            return string.Format("{0}_{1}", this.Name, this.Age).GetHashCode();
+        }
+    }
+}
