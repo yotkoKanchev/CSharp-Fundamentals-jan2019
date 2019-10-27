7724e7a6ad9ea2eb679f2f1cdfc21e57cfd1bb57
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-10-27 14:19:50 +0200

    Add files via upload

---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,41 @@
+﻿namespace P01ListyIterator
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System;
+    public class ListyIterator<T>
+    {
+        private List<T> elements;
+        private int currnetIndex;
+        public ListyIterator(params T[] elements)
+        {
+            this.elements = elements.ToList();
+            this.currnetIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (this.currnetIndex < this.elements.Count - 1)
+            {
+                this.currnetIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return this.currnetIndex < this.elements.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (!this.elements.Any())
+            {
+                throw new NullReferenceException("Invalid Operation!");
+            }
+            Console.WriteLine(this.elements[this.currnetIndex]);
+        }
+    }
+}
---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/01.ListyIterator/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace P01ListyIterator
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var elements = Console.ReadLine().Split(' ').Skip(1).ToArray();
+
+            var listyOperator = new ListyIterator<string>(elements);
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command?.ToLower() == "end")
+                {
+                    break;
+                }
+
+                if (command == "Move")
+                {
+                    Console.WriteLine(listyOperator.Move());
+                }
+                else if (command == "Print")
+                {
+                    try
+                    {
+                        listyOperator.Print();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listyOperator.HasNext());
+                }
+            }
+        }
+    }
+}
