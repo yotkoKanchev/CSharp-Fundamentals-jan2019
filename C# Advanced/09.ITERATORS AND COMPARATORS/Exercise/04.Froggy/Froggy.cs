f075c424444c6ece76a4878c6d8b0457857bfa38
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-10-27 15:40:36 +0200

    Add files via upload

---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/04.Froggy/Froggy.cs
@@ -0,0 +1,20 @@
+﻿namespace P04Froggy
+{
+    using System;
+    using System.Linq;
+
+    public class Froggy
+    {
+        public static void Main()
+        {
+            var stones = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+
+        }
+    }
+}
---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,33 @@
+﻿namespace P04Froggy
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public Lake(int[] stones)
+        {
+            this.stones = new List<int>(stones);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stones.Count; i += 2)
+            {
+                yield return this.stones[i];
+            }
+
+            int startIndex = this.stones.Count % 2 == 0 ? this.stones.Count - 1 : this.stones.Count - 2;
+
+            for (int i = startIndex; i >= 0; i-=2)
+            {
+                yield return this.stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
