09d458d67ded353e7e15a9a87b272edf9a1d5099
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-10-27 15:06:11 +0200

    Add files via upload

---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/03.Stack/CustomStack.cs
@@ -0,0 +1,58 @@
+﻿namespace P03Stack
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System;
+
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+
+        public CustomStack()
+        {
+            this.elements = new List<T>();
+        }
+        public void Push(params T[] elements)
+        {
+            this.elements.AddRange(elements);
+        }
+
+        public void Pop()
+        {
+            if (this.elements.Any())
+            {
+                this.elements.RemoveAt(this.elements.Count - 1);
+            }
+            else
+            {
+                Console.WriteLine("No elements");
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var element in this.elements)
+                {
+                    sb.AppendLine(element.ToString());
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.elements.Count - 1; i >= 0; i--)
+            {
+                yield return this.elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/03.Stack/Stack.cs
@@ -0,0 +1,35 @@
+﻿namespace P03Stack
+{
+    using System;
+    using System.Linq;
+
+    public class Stack
+    {
+        public static void Main()
+        {
+            var myCustomStack = new CustomStack<string>();
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command?.ToLower() == "end")
+                {
+                    break;
+                }
+
+                var args = command.Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
+
+                if (command.StartsWith("Pop"))
+                {
+                    myCustomStack.Pop();
+                }
+                else
+                {
+                    myCustomStack.Push(args);
+                }
+            }
+
+            Console.WriteLine(myCustomStack);
+        }
+    }
+}
