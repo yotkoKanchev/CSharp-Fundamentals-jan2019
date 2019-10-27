5e1629d50e53923d30ea978a7a083cbc21c2a288
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-10-27 14:37:19 +0200

    Add files via upload

---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/02.Collection/Collection.cs
@@ -0,0 +1,49 @@
+﻿namespace P02Collection
+{
+    using System;
+    using System.Linq;
+
+    public class Collection
+    {
+        public static void Main()
+        {
+            var elements = Console.ReadLine().Split(' ').Skip(1).ToArray();
+
+            var listyOperator = new ListyIterator<string>(elements);
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command?.ToLower() == "end")
+                {
+                    break;
+                }
+
+                try
+                {
+                    if (command == "Move")
+                    {
+                        Console.WriteLine(listyOperator.Move());
+                    }
+                    else if (command == "Print")
+                    {
+                        listyOperator.Print();
+                    }
+                    else if (command == "HasNext")
+                    {
+                        Console.WriteLine(listyOperator.HasNext());
+                    }
+                    else if (command == "PrintAll")
+                    {
+                        listyOperator.PrintAll();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/09.ITERATORS AND COMPARATORS/Exercise/02.Collection/ListyIterator.cs
@@ -0,0 +1,63 @@
+﻿namespace P02Collection
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System;
+    using System.Collections;
+
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+        private int currnetIndex;
+        public ListyIterator(params T[] elements)
+        {
+            this.elements = elements.ToList();
+            this.currnetIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (this.currnetIndex < this.elements.Count - 1)
+            {
+                this.currnetIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return this.currnetIndex < this.elements.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (!this.elements.Any())
+            {
+                throw new NullReferenceException("Invalid Operation!");
+            }
+            Console.WriteLine(this.elements[this.currnetIndex]);
+        }
+
+        public void PrintAll()
+        {
+            if (!this.elements.Any())
+            {
+                throw new NullReferenceException("Invalid Operation");
+            }
+
+            Console.WriteLine(string.Join(" ", this.elements));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var element in this.elements)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
