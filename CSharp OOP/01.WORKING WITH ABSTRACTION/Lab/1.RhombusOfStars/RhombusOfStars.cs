9759b65fdda3af627ca66c8e82c32d16b2430dec
yotkoKanchev <yotko.kanchev@gmail.com>
2019-11-01 01:09:59 +0200

    Changed folders numbering
    minor name fixes

---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Lab/1.RhombusOfStars/RhombusOfStars.cs
@@ -0,0 +1,14 @@
+﻿namespace P1RhombusOfStars
+{
+    using System;
+
+    public class RhombusOfStars
+    {
+        public static void Main()
+        {
+            var starsCount = int.Parse(Console.ReadLine());
+            var rombusDrawer = new RhombusDrawer();
+            Console.WriteLine(rombusDrawer.Draw(starsCount));
+        }
+    }
+}
---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Lab/2.PointInRectangle/PointInrectangle.cs
@@ -0,0 +1,22 @@
+﻿namespace P2PointInRectangle
+{
+    using System;
+    using System.Linq;
+
+    public class PointInRectangle
+    {
+        public static void Main()
+        {
+            var rectangle = new RectangleFactory(Console.ReadLine()).Create();
+
+            var pointsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < pointsCount; i++)
+            {
+                var point = new PointFactory(Console.ReadLine()).Create();
+                
+                Console.WriteLine(rectangle.CheckPointIsInside(point));
+            }
+        }
+    }
+}
