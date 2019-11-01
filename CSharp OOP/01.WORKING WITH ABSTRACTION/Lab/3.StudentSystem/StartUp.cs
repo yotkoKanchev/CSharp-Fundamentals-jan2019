e6ffc12a4deff166b2162d361a4d5f22df621fb8
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-11-01 11:54:18 +0200

    Add files via upload

---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Lab/3.StudentSystem/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace P03StudentSystem
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var students = new StudentDatabase();
+            var studentFactory = new StudentFactory();
+            StudentSystem studentSystem = new StudentSystem(students, studentFactory);
+            studentSystem.Execute();
+        }
+    }
+}
---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Lab/4.HotelReservation/DiscountType.cs
@@ -0,0 +1,9 @@
+﻿namespace P4HotelReservation
+{
+    public enum DiscountType
+    {
+        None = 0,
+        SecondVisit = 10,
+        VIP = 20,
+    }
+}
