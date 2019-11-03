86214523c202a0bb02985ba9daa639513a033b7d
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-11-04 00:27:46 +0200

    Add files via upload

---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Exercise/04.Hospital/Department.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Hospital
+{
+    public class Department
+    {
+        public Department(string name)
+        {
+            this.Name = name;
+            this.Rooms = new List<Room>();
+        }
+        public string Name { get; private set; }
+
+        public List<Room> Rooms { get; private set; }
+
+        public void AddPatient(Patient patient)
+        {
+            if (this.Rooms.Any())
+            {
+                var currentRoom = this.Rooms[this.Rooms.Count - 1];
+
+                if (!currentRoom.HasEmptyBed())
+                {
+                    var room = new Room(this.Rooms.Count + 1);
+                    room.Add(patient);
+                    this.Rooms.Add(room);
+                }
+                else
+                {
+                    currentRoom.Add(patient);
+                }
+            }
+            else
+            {
+                var room = new Room(this.Rooms.Count + 1);
+                room.Add(patient);
+                this.Rooms.Add(room);
+            }
+
+        }
+        public bool hasAvailableRoom()
+        {
+            return (this.Rooms.Count < 20 || this.Rooms[19].HasEmptyBed() == true);
+        }
+    }
+}
---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Exercise/04.Hospital/Room.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace P04_Hospital
+{
+    public class Room
+    {
+        public Room(int number)
+        {
+            this.Number = number;
+            this.Patients = new List<Patient>();    
+        }
+        public List<Patient> Patients { get; private set; }
+
+        public int Number { get; private set; }
+
+        public void Add(Patient patient)
+        {
+            this.Patients.Add(patient);
+        }
+        internal bool HasEmptyBed()
+        {
+            return this.Patients.Count < 3;
+        }
+    }
+}
