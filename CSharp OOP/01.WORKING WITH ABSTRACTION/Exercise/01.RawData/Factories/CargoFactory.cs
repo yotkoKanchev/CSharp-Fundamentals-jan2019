47bd6eedb35a8d9f7b500850522331555a95ce67
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-11-03 10:31:48 +0200

    Add files via upload

---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Exercise/01.RawData/Factories/CargoFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace P01RawData.Factories
+{
+    using P01RawData.Models;
+
+    public class CargoFactory
+    {
+        public Cargo Create(string[] args)
+        {
+            var weight = int.Parse(args[0]);
+            var type = args[1];
+
+            return new Cargo(weight, type);
+        }
+    }
+}
---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Exercise/01.RawData/RawData.cs
@@ -0,0 +1,50 @@
+﻿namespace P01RawData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using P01RawData.Factories;
+    using P01RawData.Models;
+
+    public class RawData
+    {
+        public static void Main()
+        {
+            var carFactory = new CarFactory();
+            int linesCount = int.Parse(Console.ReadLine());
+
+            var cars = new List<Car>();
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                var args = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                var car = carFactory.Create(args);
+
+                cars.Add(car);
+            }
+
+            var sortingCaroType = Console.ReadLine();
+
+            var result = SortCars(cars, sortingCaroType);
+
+            result?.ForEach(c => Console.WriteLine(c));
+        }
+
+        private static List<Car> SortCars(List<Car> cars, string cargoType)
+        {
+            List<Car> result = null;
+
+            if (cargoType == "fragile")
+            {
+                result = cars.Where(c => c.Cargo.Type == cargoType && c.Tires.Any(t => t.Pressure < 1)).ToList();
+            }
+            else if (cargoType == "flamable")
+            {
+                result = cars.Where(c => c.Cargo.Type == cargoType && c.Engine.Power > 250).ToList();
+            }
+
+            return result;
+        }
+    }
+}
