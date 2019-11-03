06b7eb63e286c845c9be12be524eb5725bb02cf7
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-11-03 17:45:42 +0200

    Add files via upload

---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Exercise/03.JediGalaxy/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace P03_JediGalaxy
+{
+    public class Player
+    {
+        public Player(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+    }
+}
---
+++ CSharp OOP/01.WORKING WITH ABSTRACTION/Exercise/03.JediGalaxy/StartUp.cs
@@ -0,0 +1,67 @@
+﻿namespace P03_JediGalaxy
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var playerFactory = new PlayerFactory();
+            var galaxyFactory = new GalaxyFactory();
+            long ivoPoints = 0;
+
+            var galaxy = galaxyFactory.Create(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command != "Let the Force be with you")
+            {
+                var ivo = playerFactory.Create(command);
+                var evil = playerFactory.Create(Console.ReadLine());
+
+                DestroyStars(evil, galaxy);
+                ivoPoints += AddPoints(ivo, galaxy);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(ivoPoints);
+        }
+
+        private static long AddPoints(Player ivo, Galaxy galaxy)
+        {
+            int row = ivo.Row;
+            int col = ivo.Col;
+            int points = 0;
+
+            while (row >= 0 && ivo.Col < galaxy.Matrix.GetLength(1))
+            {
+                if (galaxy.IsInside(row, col))
+                {
+                    points += galaxy.Matrix[row, col];
+                }
+
+                col++;
+                row--;
+            }
+
+            return points;
+        }
+
+        private static void DestroyStars(Player evil, Galaxy galaxy)
+        {
+            int row = evil.Row;
+            int col = evil.Col;
+
+            while (row >= 0 && col >= 0)
+            {
+                if (galaxy.IsInside(row, col))
+                {
+                    galaxy.Matrix[row, col] = 0;
+                }
+
+                row--;
+                col--;
+            }
+        }
+    }
+}
