fc730b781c735a125f27211635a706f55928a610
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-11-08 00:40:10 +0200

    Add files via upload

---
+++ CSharp OOP/02.Inheritance/Exercise/04.NeedForSpeed/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace NeedForSpeed
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var car = new Car(100, 100);
+
+            car.Drive(100);
+
+            Console.WriteLine(car.Fuel);
+        }
+    }
+}
+
