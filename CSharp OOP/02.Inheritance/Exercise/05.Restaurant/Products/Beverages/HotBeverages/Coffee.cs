4efbbf3c55fe17064b82fe4fe60e1cf821d8b8fd
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-11-08 10:12:43 +0200

    Add files via upload

---
+++ CSharp OOP/02.Inheritance/Exercise/05.Restaurant/Products/Beverages/HotBeverages/Coffee.cs
@@ -0,0 +1,15 @@
+﻿namespace Restaurant.Products.Beverages.HotBeverages
+{
+    public class Coffee : HotBeverage
+    {
+        private const double DefaultCoffeeMilliliters = 50;
+        private const decimal DefaultCoffeePrice = 3.50M;
+        public Coffee(string name, double caffeine) // the author need to specify this in the problem resource !!!
+            : base(name, DefaultCoffeePrice, DefaultCoffeeMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; internal set; }
+    }
+}
