77111ef182aa80d37d35982f9ccba19c87279ea7
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-11-14 23:01:35 +0200

    Add files via upload

---
+++ CSharp OOP/04.Interfaces and Abstraction/Exercise/05.BorderControl/Citizen.cs
@@ -0,0 +1,18 @@
+﻿namespace P05.BorderControl
+{
+    public class Citizen : IIdentyfiable
+    {
+        private string name;
+        private int age;
+        private string id;
+
+        public Citizen(string name, int age, string id)
+        {
+            this.name = name;
+            this.age = age;
+            this.Id = id;
+        }
+
+        public string Id { get; private set; }
+    }
+}
---
+++ CSharp OOP/04.Interfaces and Abstraction/Exercise/05.BorderControl/Robot.cs
@@ -0,0 +1,15 @@
+﻿namespace P05.BorderControl
+{
+    public class Robot : IIdentyfiable
+    {
+        private string model;
+        private string id;
+
+        public Robot(string model, string id)
+        {
+            this.model = model;
+            this.Id = id;
+        }
+        public string Id { get; private set; }
+    }
+}
