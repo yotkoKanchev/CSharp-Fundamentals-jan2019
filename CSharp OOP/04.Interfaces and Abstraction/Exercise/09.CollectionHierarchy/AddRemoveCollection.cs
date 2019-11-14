8f1c2cd805a1899e4379124752e79776e8e77ec7
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-11-15 01:32:38 +0200

    Add files via upload

---
+++ CSharp OOP/04.Interfaces and Abstraction/Exercise/09.CollectionHierarchy/AddRemoveCollection.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace P09.CollectionHierarchy
+{
+    public class AddRemoveCollection : Collection, IRemovable
+    {
+        public override int Add(string word)
+        {
+            this.Words.Insert(0, word);
+            return 0;
+        }
+
+        public virtual string Remove()
+        {
+            string lastElement = null;
+
+            if (this.Words.Count > 0)
+            {
+                lastElement = this.Words.Last();
+                this.Words.RemoveAt(this.Words.Count - 1);
+            }
+
+            return lastElement;
+        }
+    }
+}
