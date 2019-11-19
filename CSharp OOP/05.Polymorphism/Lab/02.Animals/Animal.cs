81269ba4feabbd4e8967a2b42a674c3ed0c9d17c
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-11-19 11:34:40 +0200

    Add files via upload

---
+++ CSharp OOP/05.Polymorphism/Lab/02.Animals/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favoriteFood;
+
+        public Animal(string name, string favoriteFood)
+        {
+            this.name = name;
+            this.favoriteFood = favoriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.name} and my fovourite food is {this.favoriteFood}";
+        }
+    }
+}
