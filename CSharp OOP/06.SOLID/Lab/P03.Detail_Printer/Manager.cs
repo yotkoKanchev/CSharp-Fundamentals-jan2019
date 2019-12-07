71f8f1ea62fe25a40bc91ce1739e5991431a3022
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-12-08 00:37:09 +0200

    Add files via upload

---
+++ CSharp OOP/06.SOLID/Lab/P03.Detail_Printer/Manager.cs
@@ -0,0 +1,27 @@
+﻿namespace P03.DetailPrinter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Manager : Employee
+    {
+        private string name;
+        public Manager(string name, ICollection<string> documents) : base(name)
+        {
+            this.Documents = new List<string>(documents);
+        }
+
+        public IReadOnlyCollection<string> Documents { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+            sb.AppendLine(string.Join(Environment.NewLine, this.Documents));
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
