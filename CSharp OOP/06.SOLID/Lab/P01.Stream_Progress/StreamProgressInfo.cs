ab7b646ec28bf94bbfad4fc7822e6b4cf8fd1fb4
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-12-08 00:15:47 +0200

    Add files via upload

---
+++ CSharp OOP/06.SOLID/Lab/P01.Stream_Progress/StreamProgressInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace P01.Stream_Progress
+{
+    public class StreamProgressInfo
+    {
+        private IStreamble stream;
+
+        // If we want to stream a music file, we can't
+        public StreamProgressInfo(IStreamble stream)
+        {
+            this.stream = stream;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.stream.BytesSent * 100) / this.stream.Length;
+        }
+    }
+}
