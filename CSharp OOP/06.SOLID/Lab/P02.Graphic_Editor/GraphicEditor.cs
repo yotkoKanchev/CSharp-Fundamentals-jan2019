80e9b0ccbc3c07fecc038bcbb46b81f3ae562c87
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-12-08 00:27:04 +0200

    Add files via upload

---
+++ CSharp OOP/06.SOLID/Lab/P02.Graphic_Editor/GraphicEditor.cs
@@ -0,0 +1,19 @@
+﻿namespace P02.Graphic_Editor
+{
+    using System;
+
+    public class GraphicEditor
+    {
+        private IShape shape;
+
+        public GraphicEditor(IShape shape)
+        {
+            this.shape = shape;
+        }
+
+        public void DrawShape(IShape shape)
+        {
+            Console.WriteLine(this.shape.Draw());
+        }
+    }
+}
---
+++ CSharp OOP/06.SOLID/Lab/P02.Graphic_Editor/Square.cs
@@ -0,0 +1,17 @@
+﻿namespace P02.Graphic_Editor
+{
+    public class Square : IShape
+    {
+        private double side;
+
+        public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public string Draw()
+        {
+            return "I'm Square";
+        }
+    }
+}
