3384c0e7cb4a9e2ef5d134558b7444faade902a0
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-12-11 09:58:05 +0200

    Add files via upload

---
+++ C# OOP/07.REFLECTION AND ATTRIBUTES/Lab/01.Stealer/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string className, params string[] fieldNames)
+    {
+        var sb = new StringBuilder();
+
+        var type = Type.GetType(className);
+        var fields = type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        var instance = Activator.CreateInstance(type);
+
+        sb.AppendLine($"Class under investigation: {className}");
+
+        foreach (var field in fields)
+        {
+            var currentFieldName = field.Name;
+            if (fieldNames.Contains(currentFieldName))
+            {
+                sb.AppendLine($"{currentFieldName} = {field.GetValue(instance)}");
+            }
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
