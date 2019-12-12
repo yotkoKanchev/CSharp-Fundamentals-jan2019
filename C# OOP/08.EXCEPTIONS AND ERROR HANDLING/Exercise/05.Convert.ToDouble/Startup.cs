418ca501d3d708cc607ac1b9d4852761dccda368
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-12-12 12:35:41 +0200

    Add files via upload

---
+++ C# OOP/08.EXCEPTIONS AND ERROR HANDLING/Exercise/05.Convert.ToDouble/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace P05.Convert.ToDouble
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var value = Console.ReadLine();
+
+            try
+            {
+                var result = Convert.ToDouble(value);
+                Console.WriteLine($"Result is : {result}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+            catch(OverflowException oe)
+            {
+                Console.WriteLine(oe.Message);
+                throw oe;
+            }
+        }
+    }
+}
