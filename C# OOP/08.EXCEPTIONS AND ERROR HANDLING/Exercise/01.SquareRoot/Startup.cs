3f5d3fb76cbcd4e2c42b918de08650822ff27da4
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-12-12 10:37:22 +0200

    Add files via upload

---
+++ C# OOP/08.EXCEPTIONS AND ERROR HANDLING/Exercise/01.SquareRoot/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace P01.SquareRoot
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            try
+            {
+                var input = Console.ReadLine();
+                var number = int.Parse(input);
+
+                if (number < 0)
+                {
+                    throw new IndexOutOfRangeException("Number can not be negative !!!");
+                }
+
+                if (!input.All(char.IsDigit))
+                {
+                    throw new FormatException("Invalid input !");
+                }
+
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch(SystemException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
