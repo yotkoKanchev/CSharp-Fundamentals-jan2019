ad39905e54531cc8065555f1be430ebf89743473
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-12-12 12:13:17 +0200

    Add files via upload

---
+++ C# OOP/08.EXCEPTIONS AND ERROR HANDLING/Exercise/03.Fixing/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace P03.Fixing
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            string[] weekdays = new string[5];
+
+            weekdays[0] = "Sunday";
+            weekdays[1] = "Monday";
+            weekdays[2] = "Tuesday";
+            weekdays[3] = "Wednesday";
+            weekdays[4] = "Thursday";
+
+            for (int i = 0; i <= 5; i++)
+            {
+                try
+                {
+                    Console.WriteLine(weekdays[i].ToString());
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
