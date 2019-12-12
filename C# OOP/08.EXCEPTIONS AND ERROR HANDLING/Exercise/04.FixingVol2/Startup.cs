6d074acc4d51113997c856e185ef19fd1256b453
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-12-12 12:21:57 +0200

    Add files via upload

---
+++ C# OOP/08.EXCEPTIONS AND ERROR HANDLING/Exercise/04.FixingVol2/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace P04.FixingVol2
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int num1, num2;
+
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{} x {} = {}", num1, num2, result);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
