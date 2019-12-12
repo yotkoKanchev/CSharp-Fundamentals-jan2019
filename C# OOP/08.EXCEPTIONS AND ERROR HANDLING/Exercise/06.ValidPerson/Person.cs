05a5f52d5284d75f5eaa451717aeaf9054588eb1
Yotko Kanchev <yotko.kanchev@gmail.com>
2019-12-12 12:54:31 +0200

    Add files via upload

---
+++ C# OOP/08.EXCEPTIONS AND ERROR HANDLING/Exercise/06.ValidPerson/Person.cs
@@ -0,0 +1,63 @@
+﻿namespace P06.ValidPerson
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName
+        {
+            get => this.firstName;
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The First Name can not be null or empty.");
+                }
+
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => this.lastName;
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The Last Name can not be null or empty.");
+                }
+
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get => this.age;
+
+            set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age should be in range [0, 120].");
+                }
+
+                age = value;
+            }
+        }
+    }
+}
---
+++ C# OOP/08.EXCEPTIONS AND ERROR HANDLING/Exercise/06.ValidPerson/Startup.cs
@@ -0,0 +1,77 @@
+﻿namespace P06.ValidPerson
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            try
+            {
+                var pesho = new Person("Pesho", "Peshev", 24);
+                Console.WriteLine($"Created Person -> {pesho.FirstName} {pesho.LastName} on {pesho.Age} years");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+
+            try
+            {
+                var personNoFirstName = new Person(string.Empty, "Peshov", 24);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+
+            try
+            {
+                var personNoLastName = new Person("Pesho", string.Empty, 24);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+
+            try
+            {
+                var personNegativeAge = new Person("Pesho", "Peshev", -1);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+
+            try
+            {
+                var personOver120Age = new Person("Pesho", "Peshev", 121);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+
+        }
+    }
+}
